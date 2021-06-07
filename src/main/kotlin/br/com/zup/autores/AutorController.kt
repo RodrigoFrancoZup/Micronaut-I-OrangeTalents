0b85a16671835d94f90eb565271790fa9e307891
Rodrigo Franco <rodrigo.franco@zup.com.br>
2021-06-07 14:24:36 -0300

    Validando os dados de entrada de uma requisição

--- src/main/kotlin/br/com/zup/autores/AutorController.kt
+++ src/main/kotlin/br/com/zup/autores/AutorController.kt
@@ -3,12 +3,15 @@ package br.com.zup.autores
 import io.micronaut.http.annotation.Body
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Post
+import io.micronaut.validation.Validated
+import javax.validation.Valid
 
 @Controller("/autores")
+@Validated
 class AutorController {
 
     @Post
-    fun cadastra(@Body request: AutorRequest){
+    fun cadastra(@Body @Valid request: AutorRequest){
         println(request)
     }
 }
