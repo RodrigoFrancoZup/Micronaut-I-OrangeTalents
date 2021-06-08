cd508975d54bea655ba02e7b0077965c8cd4ba10
Rodrigo Franco <rodrigo.franco@zup.com.br>
2021-06-08 10:51:52 -0300

    Criando busca personalizada com o Repository SEM escrever query

---
+++ src/main/kotlin/br/com/zup/autores/AutorPorEmailController.kt
@@ -0,0 +1,28 @@
+package br.com.zup.autores
+
+import io.micronaut.http.HttpResponse
+import io.micronaut.http.annotation.Controller
+import io.micronaut.http.annotation.Get
+import io.micronaut.http.annotation.QueryValue
+
+
+@Controller("/autor/email")
+class AutorPorEmailController(val autorRepository: AutorRepository) {
+
+    @Get
+    fun buscaPorEmail(@QueryValue(defaultValue = "") email: String): HttpResponse<Any> {
+        if (email.isBlank()) {
+            val autores = autorRepository.findAll()
+            val autoresResponse = autores.map { autor -> AutorResponse(autor) }
+            return HttpResponse.ok(autoresResponse)
+        }
+        val possivelAutor = autorRepository.findByEmail(email)
+        if (possivelAutor.isEmpty) {
+            return HttpResponse.notFound()
+        } else {
+            val autorResponse = AutorResponse(possivelAutor.get())
+            return HttpResponse.ok(autorResponse)
+        }
+
+    }
+}
