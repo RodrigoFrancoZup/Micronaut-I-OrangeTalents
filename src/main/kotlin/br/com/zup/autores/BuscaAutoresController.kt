b1c7143d1fae125dc8aef3b24af4eb0b900d852a
Rodrigo Franco <rodrigo.franco@zup.com.br>
2021-06-08 09:42:53 -0300

    CRUD Finalizado

---
+++ src/main/kotlin/br/com/zup/autores/BuscaAutoresController.kt
@@ -0,0 +1,16 @@
+package br.com.zup.autores
+
+import io.micronaut.http.HttpResponse
+import io.micronaut.http.annotation.Controller
+import io.micronaut.http.annotation.Get
+
+@Controller("/autores")
+class BuscaAutoresController(val autorRepository: AutorRepository) {
+
+    @Get
+    fun lista(): HttpResponse<List<AutorResponse>>? {
+        val autores = autorRepository.findAll()
+        val listaAutoresResponse = autores.map { autor -> AutorResponse(autor) }
+        return HttpResponse.ok(listaAutoresResponse)
+    }
+}
--- src/main/kotlin/br/com/zup/autores/CadastraAutorController.kt
+++ src/main/kotlin/br/com/zup/autores/CadastraAutorController.kt
@@ -1,18 +1,24 @@
 package br.com.zup.autores
 
+import io.micronaut.http.HttpResponse
 import io.micronaut.http.annotation.Body
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Post
+import io.micronaut.http.uri.UriBuilder
 import io.micronaut.validation.Validated
 import javax.validation.Valid
 
 @Controller("/autores")
 @Validated
-class AutorController ( val autorRepository: AutorRepository) {
+class CadastraAutorController (val autorRepository: AutorRepository)  {
 
     @Post
-    fun cadastra(@Body @Valid request: AutorRequest) {
+    fun cadastra(@Body @Valid request: AutorRequest) : HttpResponse<Any>{
         val autor = request.converteAutorRequestParaAutor()
         autorRepository.save(autor)
+
+        val uri = UriBuilder.of("/autores/{id}").expand(mutableMapOf(Pair("id", autor.id)))
+        return HttpResponse.created(uri)
     }
+
 }
