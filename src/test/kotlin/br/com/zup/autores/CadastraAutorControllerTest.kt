a12908fdd616b5e6a09a8436767943fe0fae7244
Rodrigo Franco <rodrigo.franco@zup.com.br>
2021-06-10 10:14:27 -0300

    Controle transacional e banco em memória - H2

--- src/test/kotlin/br/com/zup/autores/CadastraAutorControllerTest.kt
+++ src/test/kotlin/br/com/zup/autores/CadastraAutorControllerTest.kt
@@ -8,14 +8,19 @@ import io.micronaut.http.HttpStatus
 import io.micronaut.http.client.HttpClient
 import io.micronaut.http.client.annotation.Client
 import io.micronaut.test.annotation.MockBean
+import io.micronaut.test.annotation.TransactionMode
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest
-import org.junit.jupiter.api.Assertions
-import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 import org.mockito.Mockito
 import javax.inject.Inject
 
-@MicronautTest
+@MicronautTest(
+    rollback = false, //Padrão é true. Configura se cada função dará rollback após ser executada
+    transactionMode = TransactionMode.SINGLE_TRANSACTION, // Padrao é SEPARETE_TRANSACTION. Configura se cada função terá sua própria transação
+    transactional = true //O Padão já é true, mas aqui poderiamos desativar a transação! Não haveria transação na execução das funções
+)
 internal class CadastraAutorControllerTest {
 
     //Lateinit indica que será inicializado no futuro!
